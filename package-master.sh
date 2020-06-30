cdd01c9e14aa3126e7415145cc030e0dfc870c4c
Austin Vance <austin@focusedlabs.io>
2020-06-30 15:47:43 -0500

    Add pipeline-secrets chart

--- package-master.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -xe
-# git merge master
-helm lint focused/*
-helm dependency update focused/*
-helm package focused/*
-helm repo index focused --url https://focused-labs.github.io/charts
